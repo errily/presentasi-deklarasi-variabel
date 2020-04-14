1bb4c2a7b668d9eb28acce9a77643dc306c090d5
errily <erril.98@gmail.com>
2020-04-14 21:54:09 +0700

    Create literalstring2.js

---
+++ literalstring2.js
@@ -0,0 +1,22 @@
+console.log("hello world"); //tanpa ada karakter khusus
+console.log('\n');
+
+console.log("hello \bworld"); //menggunakan \b 
+console.log('\n');
+
+console.log("hello \nworld"); //menggunakan \n
+console.log('\n');
+
+console.log("hello \tworld"); //menggunakan \t
+console.log('\n');
+
+console.log("hello \'world\'"); //menggunakan \'
+console.log('\n');
+
+console.log("hello \"world\""); //menggunakan \" 
+console.log('\n');
+
+console.log("hello \\world\\"); //menggunakan \\
+console.log('\n');
+
+
