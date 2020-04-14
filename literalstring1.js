d80443c40ef42d60f72fdd16539e54a7b936694a
errily <erril.98@gmail.com>
2020-04-14 21:54:21 +0700

    Create literalstring1.js

---
+++ literalstring1.js
@@ -0,0 +1,14 @@
+var kata1 = "world";
+
+var kata2 = "hello, " + kata1 + "!";
+
+console.log(kata2); // “hello, world!”
+
+
+var kata1 = "world";
+
+var kata2 = `hello, ${kata1}!`;
+
+
+console.log(kata2); // “hello, world!”
+
