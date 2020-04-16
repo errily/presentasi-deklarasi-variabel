a7c195579498e02d6ea8bdf88805e755e7a94fe8
errily <erril.98@gmail.com>
2020-04-16 22:50:24 +0700

    Create upper.js

---
+++ upper.js
@@ -0,0 +1,4 @@
+var kalimat = "Belajar JavaScript";
+console.log(kalimat.toLocaleUpperCase());  // hasil: BELAJAR JAVASCRIPT
+console.log(kalimat.toUpperCase());        // hasil: BELAJAR JAVASCRIPT
+  
