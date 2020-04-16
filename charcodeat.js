423513a035c13a91a2760cc6fc652bfc43fce41d
errily <erril.98@gmail.com>
2020-04-16 22:49:09 +0700

    Create charcodeat.js

---
+++ charcodeat.js
@@ -0,0 +1,5 @@
+var situs = "duniailkom";
+console.log(situs.charCodeAt(0));     // 100
+console.log(situs.charCodeAt(5));     // 105
+console.log(situs.charCodeAt(7));     // 107
+console.log(situs.charCodeAt(11));    // NaN
