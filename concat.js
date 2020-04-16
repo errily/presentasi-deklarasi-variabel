6f1f003c0e7167ff91c9ad34e7eebd4faa3a7a8d
errily <erril.98@gmail.com>
2020-04-16 22:49:14 +0700

    Create concat.js

---
+++ concat.js
@@ -0,0 +1,7 @@
+var situs = "duniailkom";
+ 
+console.log(situs.concat(" oke"));            
+// duniailkom oke
+ 
+console.log(situs.concat(" situs", " belajar", " javaScript"));
+// duniailkom situs belajar javaScript
