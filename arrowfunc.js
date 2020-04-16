1a9c1ecce9f91f067ab5862b0ca8e9880f7c52f0
errily <erril.98@gmail.com>
2020-04-16 22:48:54 +0700

    Create arrowfunc.js

---
+++ arrowfunc.js
@@ -0,0 +1,11 @@
+    //penggunaan function biasa
+    const func1 = function(a, b) {
+        return a + b;
+    };
+    console.log(func1(5, 2)); //7
+    
+    //penggunaan arrow function
+    const func2 = (a, b) => {
+        return a + b;
+    };
+    console.log(func2(5, 2)); //7
