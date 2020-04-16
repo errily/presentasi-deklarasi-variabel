36ba2078852ca03de3a968752f5fa3787ff1c4bc
errily <erril.98@gmail.com>
2020-04-16 22:49:03 +0700

    Create charat.js

---
+++ charat.js
@@ -0,0 +1,5 @@
+var situs = "duniailkom";
+console.log(situs.charAt(0));     // d
+console.log(situs.charAt(5));     // i
+console.log(situs.charAt(7));     // k
+console.log(situs.charAt(11));    // string kosong
