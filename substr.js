93ede7a67a05f214b8fe32500f2f48a9baa1459f
errily <erril.98@gmail.com>
2020-04-16 22:50:04 +0700

    Create substr.js

---
+++ substr.js
@@ -0,0 +1,9 @@
+var kalimat = "Belajar JavaScript";
+ 
+// ambil mulai dari index ke 3, sebanyak 9 karakter.
+var hasil = kalimat.substr(3,9)
+console.log(hasil);    // ajar Java
+  
+// ambil mulai dari index ke 6 dari akhir string
+var hasil2 = kalimat.substr(-6)
+console.log(hasil2);    // Script
