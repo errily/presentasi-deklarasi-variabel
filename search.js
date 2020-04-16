30f7b988a267bbcbd7de711392df97cc9a8ae842
errily <erril.98@gmail.com>
2020-04-16 22:49:43 +0700

    Create search.js

---
+++ search.js
@@ -0,0 +1,3 @@
+var kalimat = "Saya sedang belajar JavaScript di Tutorial JavaScript duniailkom.com ";
+var hasil = kalimat.search(/JavaScript/g);    // cari kata "JavaScript"
+console.log(hasil);     // 20 (JavaScript berada di karakter ke-21)
