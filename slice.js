056136f96db4cbe0d9d80e0ac9e2bad7690eba6f
errily <erril.98@gmail.com>
2020-04-16 22:49:48 +0700

    Create slice.js

---
+++ slice.js
@@ -0,0 +1,9 @@
+var kalimat = "Belajar JavaScript";
+  
+// potong dari index ke 3 sampai dengan ke 7.
+var hasil = kalimat.slice(3,7)
+console.log(hasil);    // ajar
+  
+// potong dari index ke 9 dari awal sampai dengan index ke 6 dari akhir.
+var hasil2 = kalimat.slice(8,-6)
+console.log(hasil2);    // Java
