62f44be567c094eb60488924560c4eabd0f70e3e
errily <erril.98@gmail.com>
2020-04-16 22:50:14 +0700

    Create toString.js

---
+++ toString.js
@@ -0,0 +1,7 @@
+var kalimat = "Belajar JavaScript";
+console.log(kalimat.toString());      // hasil: Belajar JavaScript
+console.log(kalimat.valueOf());       // hasil: Belajar JavaScript
+  
+var kalimat2 = "bELAJAR JaVaScrIPT";
+console.log(kalimat2.toString());     // hasil: bELAJAR JaVaScrIPT
+console.log(kalimat2.valueOf());      // hasil: Belajar JavaScript
