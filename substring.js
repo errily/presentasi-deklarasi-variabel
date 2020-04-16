87584ff48e1008fa04e9f50014bbc7ca4fb2d05b
errily <erril.98@gmail.com>
2020-04-16 22:50:09 +0700

    Create substring.js

---
+++ substring.js
@@ -0,0 +1,9 @@
+var kalimat = "Belajar JavaScript";
+  
+// potong dari index ke 3 sampai dengan ke 7.
+var hasil = kalimat.substring(3,7)
+console.log(hasil);    // ajar
+  
+// potong dari index ke 8 dari awal sampai dengan akhir String.
+var hasil2 = kalimat.substring(8)
+console.log(hasil2);   // JavaScript
